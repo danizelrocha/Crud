68886adfea2fe6b0fd4fe6c9e52e92c338ca036f
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-26 10:41:03 -0300

    Js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number'); 
+const pokemonImage = document.querySelector('.pokemon_image');
+ 
+const form = document.querySelector('.form'); 
+const input = document.querySelector('.input_search'); 
+
+const fetchPokemon = async (pokemon) =>{
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
+
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+}
+
+form. addEventListener('submit', (event) =>{
+    event.preventDefault();
+    
+    renderPokemon(input.value.toLowerCase());
+    input.value = '';
+});
